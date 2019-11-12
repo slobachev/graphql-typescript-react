9aeaabad7fee9d9fd6a9f5cb3d6c23223ecd21e0
slobachev <sergeylobachev@outlook.com>
2019-11-12 17:36:43 +0200

    edit app and index

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LaunchListContainer from './components/LaunchList';
+import LaunchProfileContainer from './components/LaunchProfile';
 
-const App: React.FC = () => {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LaunchListContainer />
+      <LaunchProfileContainer />
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
-import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import { ApolloProvider as ApolloHooksProvider } from "react-apollo-hooks";
+import "./index.css";
+import App from "./App";
 
 const client = new ApolloClient({
-    uri: "https://spacexdata.herokuapp.com/graphql",
+  uri: "https://spacexdata.herokuapp.com/graphql"
 });
 
 ReactDOM.render(
-<ApolloProvider client={client}>
+  <ApolloProvider client={client}>
     <ApolloHooksProvider client={client}>
-        <App />
+      <App />
     </ApolloHooksProvider>
-</ApolloProvider>,
- document.getElementById('root'));
+  </ApolloProvider>,
+  document.getElementById("root")
+);
